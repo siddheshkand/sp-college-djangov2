b0fdffb69ddfaa4d3c6fb2275c45ede5cb6ec492
Siddhesh Kand <21078949+siddheshkand@users.noreply.github.com>
2023-05-03 13:42:23 +0530

    Forms Error Handling

--- todo/forms.py
+++ todo/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class TodoInputForm(forms.Form):
     task = forms.CharField(max_length=10)
-    desc = forms.CharField()
+    # desc = forms.CharField(max_length=5)
--- todo/views.py
+++ todo/views.py
@@ -6,14 +6,19 @@ from todo.models import Todo
 from todo.forms import TodoInputForm
 def hello_from_todo(request):
     form_for_get = TodoInputForm()
+    all_todo = Todo.objects.all()
+
+
     if request.method == 'POST':
         todo_form = TodoInputForm(request.POST)
-        print(todo_form.errors)
         if todo_form.is_valid():
             data = todo_form.cleaned_data
             Todo.objects.create(task=data['task'])
-    
-    all_todo = Todo.objects.all()
+        else:
+            # print(todo_form.errors.as_text)
+            return render(request,'todo.html',{'todos':all_todo,'form':todo_form})
+        
+
     return render(request,'todo.html',{'todos':all_todo,'form':form_for_get})
 
 def inside_todo(request):
