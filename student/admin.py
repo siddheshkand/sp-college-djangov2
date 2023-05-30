4e2942e3e47efdf9c760d462a07064c98a65793e
Siddhesh Kand <21078949+siddheshkand@users.noreply.github.com>
2023-05-30 12:07:03 +0530

    Jquery Sample

--- student/admin.py
+++ student/admin.py
@@ -1,4 +1,17 @@
 from django.contrib import admin
-from student.models import Student
+from student.models import Student,StudentBlog
 # Register your models here.
 admin.site.register(Student)
+# admin.site.register(StudentBlog)
+
+
+
+@admin.register(StudentBlog)
+class StudentBlogAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'name',
+        'description',
+        'created_at',
+        'updated_at',
+    ]
--- student/models.py
+++ student/models.py
@@ -9,3 +9,26 @@ class Student(models.Model):
     college = models.CharField(max_length=100)
     email = models.EmailField()
     profile_picture = models.ImageField(upload_to='student_images')
+
+
+class MetaFields(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    views = models.IntegerField(default=0)
+    class Meta:
+        abstract = True
+
+class StudentBlog(MetaFields):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+
+
+class TeacherBlog(MetaFields):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+
+
+class ParentsBlog(MetaFields):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+
